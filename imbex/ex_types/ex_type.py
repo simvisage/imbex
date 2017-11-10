2f9c1fb984d5a4b70a6a535281c1faff8a6dd230
campsb <bcamps@imb.rwth-aachen.de>
2017-11-10 13:30:25 +0100

    First Draft

---
+++ imbex/ex_types/ex_type.py
@@ -0,0 +1,55 @@
+__author__ = 'campsb'
+__copyright = 'Copyright 2009, IMB, RWTH Aachen'
+__date__ = 'Nov. 10, 2017'
+__status__ = 'Draft'
+
+from traits.api import HasStrictTraits, Int, Str, Float
+import pandas as pd
+
+class SpecimenDimensions(HasStrictTraits):
+
+    height = Int(unit='mm')
+
+    length = Int(unit='mm')
+
+    width = Int(unit='mm')
+
+    diameter = Int(unit='mm')
+
+    bond_length = Int(unit='mm')
+
+    number_long_ri = Int
+
+    number_stirrup = Int
+
+class Reinforcement(HasStrictTraits):
+
+    diameter_long_ri = Int(unit='mm')
+
+    diameter_stirrup = Int(unit='mm')
+
+    material_long_ri = Str('Steel')
+
+    material_stirrup = Str('Steel')
+
+    category_long_ri = Str('B500')
+
+    category_stirrup = Str('B500')
+
+    strength_long_ri = Float(unit='MPa')
+
+    strength_stirrup = Float(unit='MPa')
+
+class Concrete(HasStrictTraits):
+
+    category_c = Str('B500')
+
+    strength_c = Float(unit='MPa')
+
+    date_production = pd.to_datetime('2017-10-01')
+
+    date_test = pd.to_datetime('2017-11-01')
+
+    age_specimen = Int(unit='d')
+
+
