91490d9dd3688c036f846b03231d68265afad7f9
campsb <bcamps@imb.rwth-aachen.de>
2017-11-13 09:25:01 +0100

    input variables cached_properties

--- imbex/ex_types/cylinder_test.py
+++ imbex/ex_types/cylinder_test.py
@@ -7,8 +7,6 @@ import numpy as np
 from traits.api import\
     HasStrictTraits, Float, Str, Int, Array, Property, cached_property
 import pandas as pd
-import os
-import pylab as p
 
 # ---------------------------------------------------------------
 # definition of class for cylinder tests
@@ -99,36 +97,3 @@ class cylinder_test (HasStrictTraits):
         @cached_property
         def _get_wa3(self):
                 return self.data[:self.argmax_force, 5]
-
-
-# ---------------------------------------------------------------
-# script to analyse the data
-# ---------------------------------------------------------------
-
-
-# test = 'CT_120_1_13'
-# # directory of raw data
-# raw_data_dir=os.path.join('/users/macretina/sciebo/imb/python', 'rawdata_CT', '%s.csv' %(test))
-#
-# def decimal_comma (value):
-#         value = value.replace(',','.')
-#         return float(value)
-#
-# data = np.loadtxt(raw_data_dir, dtype=np.float, skiprows=2, converters={0: decimal_comma, 1: decimal_comma, 2: decimal_comma, 3: decimal_comma, 4: decimal_comma,5: decimal_comma}, delimiter=';')
-#
-# ct = cylinder_test(data=data)
-#
-# ax1 = p.subplot(2,3,1)
-# ax2 = p.subplot(2,3,2)
-# ax3 = p.subplot(2,3,3)
-# ax4 = p.subplot(2,3,4)
-#
-#
-# ax1.plot(ct.t, ct.f)
-# ax2.plot(ct.u, ct.f)
-# ax2.plot(ct.wa1, ct.f)
-# ax2.plot(ct.wa2, ct.f)
-# ax2.plot(ct.wa3, ct.f)
-#
-# p.show()
-
