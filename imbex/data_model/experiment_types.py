6282bbf67a646a826e06f52d168084efa42024ca
campsb <32643894+campsb@users.noreply.github.com>
2017-11-30 15:26:22 +0100

    minor improvements

--- imbex/data_model/experiment_types.py
+++ imbex/data_model/experiment_types.py
@@ -3,20 +3,24 @@ __copyright = 'Copyright 2009, IMB, RWTH Aachen'
 __date__ = 'Nov. 23, 2017'
 __status__ = 'Draft'
 
-import paramiko
-from traitsui.api import View, Item, Group
-from traitsui.menu import OKButton, CancelButton
 
+# imports
 import numpy as np
 import pandas as pd
+import paramiko
 import traits.api as tr
+from traitsui.api import View, Item, Group, Handler, EnumEditor
+from traitsui.menu import OKButton, CancelButton
 
 
 # -----------------------------------------------------------------------------
 # definition of classes for the different experiment types, materials, methods
 # -----------------------------------------------------------------------------
-# class defining the beam-end tests
+
+
 class BeamEndTest(tr.HasStrictTraits):
+    """ Defines the Beam-End test class """
+
     height = tr.Float(unit='mm')
 
     length = tr.Float(unit='mm')
@@ -38,8 +42,9 @@ class BeamEndTest(tr.HasStrictTraits):
                              label='Beam End', show_border=True))
 
 
-# class defining the reinforcement
 class Reinforcement(tr.HasStrictTraits):
+    """ Defines the Reinforcement class """
+
     diameter_long_ri = tr.Int(unit='mm')
 
     diameter_stirrup = tr.Int(unit='mm')
@@ -57,8 +62,9 @@ class Reinforcement(tr.HasStrictTraits):
     strength_stirrup = tr.Float(unit='MPa')
 
 
-# class defining the concrete
 class Concrete(tr.HasStrictTraits):
+    """ Defines the Concrete class """
+
     category_c = tr.Str('C120')
 
     strength_c = tr.Float(unit='MPa')
@@ -70,8 +76,9 @@ class Concrete(tr.HasStrictTraits):
     age_specimen = tr.Int(unit='d')
 
 
-# class defining the cylinder tests
 class CylinderTest(tr.HasStrictTraits):
+    """ Defines the Cylinder Test class """
+
     # input variables
     data = tr.Array(np.float_, input=True)
 
@@ -147,67 +154,134 @@ class CylinderTest(tr.HasStrictTraits):
         return self.data[:self.argmax_force, 5]
 
 
-# class for SFTP connection using paramiko
 class SFTPConnection(tr.HasTraits):
-    # input variables
-    username = tr.Str('ftp',
-                      desc="username",
-                      label="username", )
+    """ Defines the SFTP connection class """
 
-    password = tr.Password(desc="password",
-                           label="password", )
+    # input variables
+    username = tr.Str('ftp', desc="username", label="username", )
+
+    # tr.Password hides the entered password
+    password = tr.Password('!mb1@FTP7', desc="password", label="password", )
+
+    test_types = tr.Enum('Cylinder-Tests', 'Beam-End-Tests', 'Stress-Redistribution-Tests')
 
     # definition of pop-up window
     traits_view = View(Item(name='username'),
-                       Item(name='password', style='simple', label='Password'),
+                       Item(name='password'),
+                       Item(name='test_types'),
                        buttons=[OKButton, CancelButton])
 
     # prints the entered username and password
-    def check_authentication(self):
-        print('Loged in as user "%s" with password "%s".' % (
-            self.username, self.password))
+    def check_inputs(self):
+        print('Logged in as user: "%s"' % self.username)
+        print('Requested test type: %s' % self.test_types)
 
-    # returns a SFTP-Connection with given arguments
-    def __init__(self, test, filepath, localpath):
-        self.test = test
+    def output_test_type(self):
+        return self.test_types
+
+    def requested_directory(self):
+        return '/home/ftp/austausch_chudoba/%s/raw_data' % self.test_types
+
+    # variables of the object(self), do not belong to the class, changeable for every element
+    def __init__(self):
         self.host = "134.130.81.25"
         self.port = 22
-        self.filepath = filepath
-        self.localpath = localpath
+        self.transport = paramiko.Transport(self.host, self.port)
+        self.transport.connect(username=self.username, password=self.password)
+        self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+
+    def list_files(self, directory):
+        self.directory = directory
+        self.file_list = self.sftp.listdir(self.directory)
+        return self.file_list
 
     # returns the path of the local file after downloading it from the server
-    def connect(self):
-        global sftp
-        global transport
-        transport = paramiko.Transport(self.host, self.port)
-        transport.connect(username=self.username, password=self.password)
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        sftp.get(self.filepath, self.localpath)
+    def transport_file(self, test_file, test):
+        self.filepath = '/home/ftp/austausch_chudoba/%s/raw_data/%s' %(test_file, test)
+        self.localpath = './%s' % test
+        # get the raw data file from the server and store it local
+        self.sftp.get(self.filepath, self.localpath)
+        # return the path of the local raw data file
         return self.localpath
 
-    # function to disconnect
+    def local_raw_file(self):
+        # return only the path of the local raw data file created in transport_file
+        return self.localpath
+
     def disconnect(self):
-        return [sftp.close(), transport.close()]
+        # disconnects from the ftp server
+        return [self.sftp.close(), self.transport.close()]
 
 
 class RequestedTest(tr.HasTraits):
-    '''class defining the requested test
-    '''
-    test_name = tr.Str('CT_120_1_', desc="test name",
-                       label="test name", )
-    '''input variable
-    '''
+    """ Defines the requested test class """
+
+    file_list = ()
+    list = tr.Enum(values='file_list')
 
     # definition of pop-up window
-    traits_view = View(Item(name='test_name'),
+    traits_view = View(Item(name='list'),
                        buttons=[OKButton, CancelButton])
 
     # returns the global variable test
     def output_test(self):
-        global test
-        test = self.test_name
-        return test
+        return self.list
 
     # prints the entered test name
-    def check_test_name(self):
-        print('Requested test: %s' % self.test_name)
+    def check_input(self):
+        print('Requested test: %s' % self.list)
+
+
+
+# class TestType(tr.HasTraits):
+#     ''' class for dynamic redefinition of test list
+#     '''
+#     CT_list = tr.List()
+#     BE_list = tr.List()
+#     SR_list = tr.List()
+#
+#     test_type_list = {
+#         'Cylinder Tests': CT_list,
+#         'Beam-End Tests': BE_list,
+#         'Stress-Redistribution Tests': SR_list
+#     }
+#     test_type = tr.Enum(list(test_type_list())[0], list(test_type_list()))
+#     test = tr.Str
+#
+#     view = View(
+#         Item(name='test_type_list'),
+#         Item(name='test',
+#              editor=EnumEditor(name='handler.test_list'),
+#              ),
+#         title='Test type information',
+#         buttons=['OK'],
+#         resizable = True,
+#         handler=TestHandler
+#     )
+#     # test_type = Enum()
+#     def list_output(self):
+#         return self.CT_list
+#
+#
+# class TestHandler(Handler):
+#     """
+#     Handler class to redefine the possible values of 'city' based on 'state'.
+#     This handler will be assigned to a view of an Address, and can listen and
+#     respond to changes in the viewed Address.
+#     """
+#
+#     # Current list of available cities:
+#     test_list = tr.List(tr.Str)
+#
+#     def object_test_type_changed(self, info):
+#         """
+#         This method listens for a change in the *state* attribute of the
+#         object (Address) being viewed.
+#         When this listener method is called, *info.object* is a reference to
+#         the viewed object (Address).
+#         """
+#         # Change the list of available cities
+#         self.tests = tests[info.object.test_type]
+#
+#         # As default value, use the first city in the list:
+#         info.object.test = self.tests[0]
